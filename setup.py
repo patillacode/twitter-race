9d6b75b77ed5218791b4cfbcdf8e10938ba8092f
Michael Klich <michal@michalklich.com>
2015-12-05 23:13:19 +0000

    Add setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='twitter-race',
+    version='0.0.1',
+    description='',
+    license='GPLv2',
+    py_modules=['track'],
+    author='patillacode',
+    author_email='patillacode@gmail.com',
+    url='https://github.com/patillacode/twitter-race',
+    install_requires=['tweepy>=3.5.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v2',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7']
+)
