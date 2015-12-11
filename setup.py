2dc68428434374b58c611f0b1515d826b93a9cd4
patillacode <patillacode@gmail.com>
2015-12-11 23:58:59 +0000

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='twitter-race',
-    version='0.1.0',
+    version='0.2.0',
     description='',
     license='GPLv2',
     packages=find_packages(),
