88a98647892e348c9c0d3736ab2bfda738ffb852
patillacode <patillacode@gmail.com>
2015-12-11 20:26:05 +0000

    Added redis to setup.py file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='patillacode',
     author_email='patillacode@gmail.com',
     url='https://github.com/patillacode/twitter-race',
-    install_requires=['tweepy>=3.5.0', 'bumpversion>=0.5.3'],
+    install_requires=['tweepy>=3.5.0', 'bumpversion>=0.5.3', 'redis>=2.10.5'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
